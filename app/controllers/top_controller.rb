ac779c6b517526e7f517bc0c3a49aeea55b7bc53
Ryosuke Tezuka <nanairo@RyosukenoMacBook-Pro.local>
2019-05-18 14:09:02 +0900

    [Add]MVCの作成

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,9 @@
+class TopController < ApplicationController
+
+	def home
+	end
+
+	def about
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
+  root to: 'top#home'
+  get 'about' => 'top#about'
+
 end
